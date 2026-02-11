0c23d6cc2274a38216bf6c0f24bd3b6eafcc8a58
Undela Kavya Saranya <181745091+KavyaUndela@users.noreply.github.com>
2026-02-11 22:04:30 +0530

    Time: 9 ms (42.17%), Space: 42.5 MB (13.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,13 +3,7 @@ class Solution {
         if(n<=1){
             return n;
         }
-        int a=0;
-        int b=1;
-        for(int i=2;i<=n;i++){
-            int sum=a+b;
-            a=b;
-            b=sum;
-        }
-        return b;
+        
+        return fib(n-1)+fib(n-2);
     }
 }
