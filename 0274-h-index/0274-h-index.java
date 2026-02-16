81070a2ac3cea9d04a9279011019514c724fdbf2
Undela Kavya Saranya <181745091+KavyaUndela@users.noreply.github.com>
2026-02-16 23:33:18 +0530

    Time: 5 ms (68.82%), Space: 43.9 MB (13.34%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        for(int i=0;i<citations.length;i++){
+            int paper=citations.length-i;
+                     if(citations[i]>=paper){
+                        return paper;
+                     }
+        }
+        return 0;
+    }
+}
