0b80623b7c367c08ba2d0bf677db6c7774da8e69
Undela Kavya Saranya <181745091+KavyaUndela@users.noreply.github.com>
2026-03-10 19:46:02 +0530

    Time: 3 ms (14.38%), Space: 42.8 MB (42.07%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String toHex(int num) {
+        
+        if(num == 0) return "0";
+
+        char[] hex = {'0','1','2','3','4','5','6','7','8','9',
+                      'a','b','c','d','e','f'};
+
+        String ans = "";
+
+        while(num != 0){
+            int rem = num & 15;  
+            ans = hex[rem] + ans;
+            num >>>= 4;         
+        }
+
+        return ans;
+    }
+}
